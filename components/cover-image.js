9ac37df10f5ac4874b9ef90efa83942d49c6c5d8
microdotmatrix <liquid.knives.2@gmail.com>
2022-06-07 13:07:48 -0500

    its alive!!

--- components/cover-image.js
+++ components/cover-image.js
@@ -6,10 +6,10 @@ export default function CoverImage({ title, coverImage, slug }) {
   const image = (
     <Image
       width={2000}
-      height={1000}
+      height={600}
       alt={`Cover Image for ${title}`}
       src={coverImage?.node.sourceUrl}
-      className={cn('shadow-small', {
+      className={cn('shadow-small object-cover', {
         'hover:shadow-medium transition-shadow duration-200': slug,
       })}
     />
